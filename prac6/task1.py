151a4d44bc4f8fe1a9aa6b6ca3b0f50c8a3be45b
Дмитрий Лисютин <54859565+SelectionForCollection@users.noreply.github.com>
2022-05-12 17:26:36 +0300

    Update task1.py

--- prac6/task1.py
+++ prac6/task1.py
@@ -1,9 +1,7 @@
+""" >>> deriv(lambda x: x ** 3)(5) 75.00014999664018 """
+
 def deriv(f, dx=0.001):
     return lambda x: (f(x + dx) - f(x)) / dx
-
-
-def f3(x):
-    return x ** 3
 
 
 print(deriv(lambda x: x ** 3)(5))
